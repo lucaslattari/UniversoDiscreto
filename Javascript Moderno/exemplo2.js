f0c3eadf14a1fe72848868b2daebf7e6288c7539
Lucas Lattari <lucas.lattari@ifsudestemg.edu.br>
2018-10-18 19:33:17 -0300

    Add files via upload

---
+++ Javascript Moderno/exemplo2.js
@@ -0,0 +1,17 @@
+/*por muito tempo o javascript evoluiu sem
+maiores preocupações com compatibilidade.
+Novas estruturas foram adicionadas sem
+alterar recursos antigos.
+
+O ecossistema de JS funcionava dessa forma
+até 2009, quando surgiu o ECMAScript 5. Esse
+padrão instituiu novos recursos e alterou
+alguns que existiam. Para ativar esses
+novos recursos explicitamente, colocamos a
+string "use strict" no topo do script JS
+*/
+
+"use strict";
+
+/*a partir daqui todo nosso código funciona
+com padrões modernos*/
