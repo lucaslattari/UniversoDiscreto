afc9150034099856a0733e8f4582d41212bfc9c6
Lucas Lattari <lucas.lattari@ifsudestemg.edu.br>
2018-11-14 13:44:54 -0200

    Add files via upload

---
+++ Javascript Moderno/Episódio 1/exemplo2.js
@@ -0,0 +1,29 @@
+/*por muito tempo o javascript evoluiu sem
+maiores preocupações com compatibilidade.
+Novas estruturas foram adicionadas sem
+alterar recursos antigos.
+
+O ecossistema de JS funcionava dessa forma
+até 2009, quando surgiu o ECMAScript 5. Esse
+padrão instituiu novos recursos e alterou
+alguns que existiam. Para ativar esses
+novos recursos explicitamente, colocamos a
+string "use strict" no topo do script JS
+*/
+
+"use strict";
+
+/*a partir daqui todo nosso código funciona
+com padrões modernos
+
+é preciso garantir que o use strict esteja no
+topo do arquivo, caso contrário o modo restrito
+não estará ativado.
+
+Deixando bastante claro, a introdução do use strict
+faz com que o motor JS seja modificado para o modo
+moderno, mudando o comportamento de recursos existentes
+
+Esse recurso funciona em todos os navegadores modernos
+e é sempre recomendado que os scripts comecem com essa
+string*/
