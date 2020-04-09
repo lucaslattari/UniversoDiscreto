baadbc8922857ec276754f58a11f34ca6bdba2e6
Leonardo Persan <leopso1990@gmail.com>
2020-04-09 17:23:33 -0300

    refactor: loc to associated columns

--- Machine Learning/Ep 1/missingdata.py
+++ Machine Learning/Ep 1/missingdata.py
@@ -2,12 +2,10 @@ import numpy as np
 import pandas as pd
 
 baseDeDados = pd.read_csv('svbr.csv', delimiter=';')
-X = baseDeDados.iloc[:,:].values
 
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='median')
-imputer = imputer.fit(X[:,1:3])
-X = imputer.transform(X[:,1:3]).astype(str)
-X = np.insert(X, 0, baseDeDados.iloc[:,0].values, axis=1)
+imputer = imputer.fit(baseDeDados.loc[:,('Inscritos','Visualizações')])
+baseDeDados.loc[:,('Inscritos','Visualizações')] = imputer.transform(baseDeDados.loc[:,('Inscritos','Visualizações')]).astype(str)
 
-print(X)
+print(baseDeDados)
